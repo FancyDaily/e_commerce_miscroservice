ffc4c061867c5dd81f8d7bfd63a72f57497d86ee
nessary <nessary@foxmail.com>
2019-02-28 13:16:20 +0000

    Update auto_developer_miscroserver_server.sh

--- auto_developer_miscroserver_server.sh
+++ auto_developer_miscroserver_server.sh
@@ -1,4 +1,5 @@
 `mkdir ~/.ssh/`
+
 cat <<EOF >~/.ssh/id_rsa.pub 
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL0oKnUxSC0ngCeyjspe6ZSDDMELnypnrHO0rk/mh7qQ/4DhPLjjfeAphr/xvaH4jC8H7xZKla2/JjuX8EQ1jpaIwjDAK8Fi7j2cbJ62QQ6mu2dHPSlzcZqewWdMhkQ78yvXv5TwqnQ4sLGslNZhxQcpwgO1zTB69YQhOMW/JYyM/nJszXBnSpyxc2iE7ZZe9dMFMsbhPiY5kKet+paK57gb0QlpIm7kbPMV1j8cKtJkCuVchyvIwhrdM2J0zMlOhFhpdHhdHo9JZicnZRnBkM4DvbfYYRyQ4PMWtB7JjWbYw+l/9pM2nR4S4BcfOZ0SPjtgAPmunXk+++YmuH8xUB root@192.168.1.3
 EOF
@@ -33,9 +34,10 @@ Uh2hLRwE6/eBJ+7Bl6PPZXQyxsQ4rmReoZeJr4cge3lK8FraaAD6tw==
 -----END RSA PRIVATE KEY-----
 EOF
 
-remain=`cat ~/.ssh/id_rsa`
-echo $remain
-remain=`cat ~/.ssh/id_rsa.pub`
-echo $remian
+remain_rsa=`cat ~/.ssh/id_rsa`
+echo $remain_rsa
+echo "________________________"
+remain_pub=`cat ~/.ssh/id_rsa.pub`
+echo $remian_pub
 ss=`ssh root@yjj.nessary.top "ls ~/"`
 echo $ss
