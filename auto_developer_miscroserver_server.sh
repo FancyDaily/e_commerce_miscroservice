06b0a7b7f5d90685e4251914d212c75d72d3b5f5
nessary <nessary@foxmail.com>
2019-03-01 09:09:08 +0000

    Update auto_developer_miscroserver_server.sh

--- auto_developer_miscroserver_server.sh
+++ auto_developer_miscroserver_server.sh
@@ -1,3 +1,3 @@
-ss=`ssh -o StrictHostKeyChecking=no -i ./.ssh/id_rsa  root@timer.nessary.top "ls -al /opt/"`
+ss=`ssh -o StrictHostKeyChecking=no -i ./.ssh/id_rsa  -p 11 root@timer.nessary.top "ls -al /opt/"`
 echo $ss
 
