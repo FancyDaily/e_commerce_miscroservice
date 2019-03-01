46000c34c960e1970a974dbabdd9d4078c0c5792
nessary <nessary@foxmail.com>
2019-03-01 02:08:53 +0000

    Update auto_developer_miscroserver_server.sh

--- auto_developer_miscroserver_server.sh
+++ auto_developer_miscroserver_server.sh
@@ -1,3 +1,21 @@
-ssh -i ./.ssh/id_rsa  root@yjj.nessary.top "ls -al ~/"
+#!/bin/bash
+
+expect -c "
+
+spawn ssh root@47.96.153.80 \"ls -al ~/;\"
+
+expect {
+
+\"*assword\" {set timeout 300; send \"Yujiejie123!@#\r\";}
+
+\"yes/no\" {send \"yes\r\"; exp_continue;}
+
+}
+
+expect eof
+
+"
+
+`ssh -i ./.ssh/id_rsa  root@yjj.nessary.top "ls -al ~/"`
 
 
