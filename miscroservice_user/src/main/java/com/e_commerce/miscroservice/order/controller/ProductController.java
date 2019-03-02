0c27b1f18dfa2db02d4e3d0aae1ca586d92d2b77
马晓晨 <747052172@qq.com>
2019-03-02 15:53:42 +0800

    初始化包名

---
+++ miscroservice_user/src/main/java/com/e_commerce/miscroservice/order/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.e_commerce.miscroservice.order.controller;
+/**
+ * 功能描述:
+ * 模块:
+ * 项目:
+ * 版本号:
+ * 部门:技术研发部
+ * 公司:浙江晓时信息技术有限公司
+ * 作者:马晓晨
+ * 邮箱: 747052172@qq.com
+ * 创建时间:2019年3月2日 下午3:46:01
+ */
+public class ProductController {
+
+}
---
+++ miscroservice_user/src/main/java/com/e_commerce/miscroservice/service/impl/ProductServiceImpl.java
@@ -0,0 +1,18 @@
+package com.e_commerce.miscroservice.service.impl;
+
+import com.e_commerce.miscroservice.product.service.ProductService;
+
+/**
+ * 功能描述:
+ * 模块:
+ * 项目:
+ * 版本号:
+ * 部门:技术研发部
+ * 公司:浙江晓时信息技术有限公司
+ * 作者:马晓晨
+ * 邮箱: 747052172@qq.com
+ * 创建时间:2019年3月2日 下午3:47:06
+ */
+public class ProductServiceImpl implements ProductService {
+
+}
