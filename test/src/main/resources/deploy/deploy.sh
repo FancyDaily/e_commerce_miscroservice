346ddefd2ceb7ff52fdc157bd3ed24ad08da36c3
nessary <nessary@foxmail.com>
2019-03-04 19:48:05 +0800

    修正demo

--- test/src/main/resources/deploy/deploy.sh
+++ test/src/main/resources/deploy/deploy.sh
@@ -1,2 +1,2 @@
-ss=`ssh -o StrictHostKeyChecking=no  -i ./test/src/main/resources/deploy/id_rsa  -p 11 root@timer.nessary.top "ls -al /opt/"`
-echo $ss
+result=`ssh -o StrictHostKeyChecking=no  -i ./test/src/main/resources/deploy/id_rsa  -p 11 root@timer.nessary.top "sh /opt/deploy/deploy_application.sh"`
+echo $result
