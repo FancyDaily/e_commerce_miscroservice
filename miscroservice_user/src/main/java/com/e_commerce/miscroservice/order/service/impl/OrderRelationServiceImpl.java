458209310822360c7e00bc36aecfd519a68b8c66
Arthes-hong <414368243@qq.com>
2019-04-09 17:55:54 +0800

    短信

--- miscroservice_user/src/main/java/com/e_commerce/miscroservice/order/service/impl/OrderRelationServiceImpl.java
+++ miscroservice_user/src/main/java/com/e_commerce/miscroservice/order/service/impl/OrderRelationServiceImpl.java
@@ -2126,7 +2126,7 @@ public class OrderRelationServiceImpl extends BaseService implements OrderRelati
         TReport report = new TReport();
         report.setReportUserId(toUser.getId());
         report.setType(ReportEnum.TYPE_ORDER.getType());
-        report.setAssociationId(orderRelationship.getId());
+        report.setAssociationId(orderRelationship.getOrderId());
         report.setLabelsId(labelsId);
         report.setMessage(message);
         report.setVoucherUrl(voucherUrl);
