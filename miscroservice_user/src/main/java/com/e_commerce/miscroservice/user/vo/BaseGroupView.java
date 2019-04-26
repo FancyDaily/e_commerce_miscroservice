e434f1872bc4d8a3cc47bbaa49d77f4dd57d6274
FancyDaily <fancydaily@163.com>
2019-04-26 17:31:04 +0800

    Merge branch 'master' of gitlab.com:nessary/xiaoshi_java
    # Conflicts:
    #	miscroservice_user/src/main/java/com/e_commerce/miscroservice/order/service/impl/OrderServiceImpl.java

--- miscroservice_user/src/main/java/com/e_commerce/miscroservice/user/vo/BaseGroupView.java
+++ miscroservice_user/src/main/java/com/e_commerce/miscroservice/user/vo/BaseGroupView.java
@@ -2,6 +2,7 @@ package com.e_commerce.miscroservice.user.vo;
 
 
 import com.e_commerce.miscroservice.commons.entity.application.TGroup;
+import lombok.Data;
 
 /**
  * 功能描述:
@@ -14,15 +15,40 @@ import com.e_commerce.miscroservice.commons.entity.application.TGroup;
  * 邮箱: 747052172@qq.com
  * 创建时间:2019年1月16日 下午2:43:09
  */
+@Data
 public class BaseGroupView extends TGroup {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 4226493331327630638L;
 	/**
 	 * 该组织下的人数
 	 */
+    private Long id;
+
+    private Long companyId;
+
+    private String groupName;
+
+    private Integer auth;
+
+    private String extend;
+
+    private Long createUser;
+
+    private String createUserName;
+
+    private Long createTime;
+
+    private Long updateUser;
+
+    private String updateUserName;
+
+    private Long updateTime;
+
+    private String isValid;
+
 	private Integer personNum = 0;
 
 	public Integer getPersonNum() {
@@ -36,7 +62,100 @@ public class BaseGroupView extends TGroup {
 	public String getIdString() {
 		return getId() + "";
 	}
-	
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(Long companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName == null ? null : groupName.trim();
+	}
+
+	public Integer getAuth() {
+		return auth;
+	}
+
+	public void setAuth(Integer auth) {
+		this.auth = auth;
+	}
+
+	public String getExtend() {
+		return extend;
+	}
+
+	public void setExtend(String extend) {
+		this.extend = extend == null ? null : extend.trim();
+	}
+
+	public Long getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(Long createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getCreateUserName() {
+		return createUserName;
+	}
+
+	public void setCreateUserName(String createUserName) {
+		this.createUserName = createUserName == null ? null : createUserName.trim();
+	}
+
+	public Long getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+
+	public Long getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(Long updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public String getUpdateUserName() {
+		return updateUserName;
+	}
+
+	public void setUpdateUserName(String updateUserName) {
+		this.updateUserName = updateUserName == null ? null : updateUserName.trim();
+	}
+
+	public Long getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Long updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getIsValid() {
+		return isValid;
+	}
+
+	public void setIsValid(String isValid) {
+		this.isValid = isValid == null ? null : isValid.trim();
+	}
 }
