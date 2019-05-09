af8eeb2ccbffb44651b4bbdcbb002233a300b2aa
nessary <nessary@foxmail.com>
2019-05-09 11:43:18 +0800

    修复配置

---
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/order/controller/Upload.java
@@ -0,0 +1,7 @@
+package com.e_commerce.miscroservice.order.controller;
+
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
