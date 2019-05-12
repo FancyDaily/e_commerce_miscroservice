4eab0973bcd666479833114b05ef74434dcc7643
FancyDaily <fancydaily@163.com>
2019-05-12 18:37:39 +0800

    Revert: fix: 订单列表 正在学习课程列表

---
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/order/controller/FileUrlManagers.java
@@ -0,0 +1,284 @@
+package com.e_commerce.miscroservice.order.controller;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.alidns.model.v20150109.*;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.springframework.security.access.method.P;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 文件url管理
+ */
+@Component
+public class FileUrlManagers {
+
+    private Upload upload = new Upload();
+    private Dns dns = new Dns();
+
+
+    /**
+     * 推送
+     *
+     * @param fileName 文件名
+     */
+    public Boolean push(String fileName) {
+        Boolean pushSuccessFlag=Boolean.FALSE;
+        if(fileName==null||fileName.isEmpty()){
+            return pushSuccessFlag;
+        }
+        try {
+            upload.upload(fileName);
+            pushSuccessFlag=Boolean.TRUE;
+
+        } catch (QiniuException e) {
+
+        }
+        return pushSuccessFlag;
+    }
+
+    /**
+     * 根据文件名 获取地址
+     *
+     * @param fileName 文件名称
+     * @return
+     */
+    public String getUrl(String fileName) {
+        String url = "";
+        if(fileName==null||fileName.isEmpty()){
+            return url;
+        }
+        try {
+            String dnsName = System.currentTimeMillis() + "";
+            if (dns.addDnsByName(dnsName)) {
+                url = dns.getUrlByName(dnsName, fileName);
+            }
+
+        } catch (ClientException e) {
+
+        }
+        return url;
+    }
+
+
+    /**
+     * dns
+     */
+    private static class Dns {
+
+        private IAcsClient client;
+        private final Long TASK_EXEC_INTERVAL_MILL_TIME = 1000 * 60L;
+        private final Long TASK_INVALID_MILL_TIME = 1000 * 60 * 10L;
+        private final String DNS_DOMAIN_NAME = "xiaoshitimebank.com";
+        private volatile Boolean IS_START = Boolean.FALSE;
+        private final String ACCESS_URL_PREFIX = "http://%s." + DNS_DOMAIN_NAME + "/%s";
+        private final String DNS_VAL_PARSE_DOMAIN_NAME = "xiaoshitimebank.com.www.qiniudns.com";
+
+        private static Map<String, Long> CDN_ID_CACHE = new ConcurrentHashMap<>();
+
+
+        {
+            init();
+        }
+
+
+        /**
+         * 初始化任务
+         */
+        private synchronized void init() {
+
+            if (IS_START) {
+                return;
+            } else {
+                IS_START = Boolean.TRUE;
+            }
+
+            initClient();
+
+            initTimer();
+            clearAll();
+
+        }
+
+        /**
+         * 初始化客户端参数
+         */
+        private void initClient() {
+            String regionId = "cn-hangzhou";
+            String accessKeyId = "LTAIugb9g6sfhKNs";
+            String accessKeySecret = "gOqhLzaGYZ09nAi5SrTouN4to1fsU5";
+            IClientProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
+            client = new DefaultAcsClient(profile);
+        }
+
+
+        /**
+         * 初始化定时任务
+         */
+        private void initTimer() {
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                    try {
+                        clearInvalidByCache();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }, TASK_EXEC_INTERVAL_MILL_TIME, TASK_EXEC_INTERVAL_MILL_TIME);
+        }
+
+
+        /**
+         * 清理缓存中失效的key
+         *
+         * @throws ClientException
+         */
+        private void clearInvalidByCache() throws ClientException {
+            for (Map.Entry<String, Long> entry : CDN_ID_CACHE.entrySet()) {
+                if (System.currentTimeMillis() - entry.getValue() > TASK_INVALID_MILL_TIME) {
+                    CDN_ID_CACHE.remove(entry.getKey());
+                    removeDnsById(entry.getKey());
+
+                }
+            }
+
+        }
+
+
+        /**
+         * 清除所有
+         */
+        private void clearAll() {
+            try {
+                DescribeDomainRecordsRequest allRecords = new DescribeDomainRecordsRequest();
+                allRecords.setActionName("DescribeDomainRecords");
+                allRecords.setDomainName(DNS_DOMAIN_NAME);
+                allRecords.setPageSize(500L);
+                allRecords.setValueKeyWord(DNS_VAL_PARSE_DOMAIN_NAME);
+                DescribeDomainRecordsResponse recordsResponse = client.getAcsResponse(allRecords);
+                for (DescribeDomainRecordsResponse.Record record : recordsResponse.getDomainRecords()) {
+                    removeDnsById(record.getRecordId());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        /**
+         * 根据Id删除dns记录值
+         *
+         * @param recordId Id
+         * @throws ClientException
+         */
+        private void removeDnsById(String recordId) throws ClientException {
+            DeleteDomainRecordRequest deleteDomainRecordRequest = new DeleteDomainRecordRequest();
+            deleteDomainRecordRequest.setActionName("DeleteDomainRecord");
+            deleteDomainRecordRequest.setRecordId(recordId);
+            client.doAction(deleteDomainRecordRequest, true, 3);
+        }
+
+
+        /**
+         * 根据名称添加dns记录值
+         *
+         * @param dnsName dns名称
+         * @throws ClientException
+         */
+        public Boolean addDnsByName(String dnsName) throws ClientException {
+            AddDomainRecordRequest addDomainRequest = new AddDomainRecordRequest();
+            addDomainRequest.setActionName("AddDomainRecord");
+            addDomainRequest.setDomainName(DNS_DOMAIN_NAME);
+            addDomainRequest.setRR(dnsName);
+            addDomainRequest.setType("CNAME");
+            addDomainRequest.setValue(DNS_VAL_PARSE_DOMAIN_NAME);
+            addDomainRequest.setTTL(600L);
+            AddDomainRecordResponse acsResponse = client.getAcsResponse(addDomainRequest);
+
+            if (acsResponse.getRecordId() != null) {
+                CDN_ID_CACHE.put(acsResponse.getRecordId(), System.currentTimeMillis());
+                return Boolean.TRUE;
+            } else {
+                return Boolean.FALSE;
+            }
+
+        }
+
+        /**
+         * 根据dns名称和文件名称获取链接地址
+         *
+         * @param dnsName  dns名称
+         * @param fileName 文件名称
+         * @return
+         */
+        public String getUrlByName(String dnsName, String fileName) {
+            return String.format(ACCESS_URL_PREFIX, dnsName, fileName);
+        }
+
+
+    }
+
+
+    /**
+     * 上传
+     */
+
+    private class Upload {
+
+
+        private final String FILE_TOTAL_PATH = "/Users/ness/Downloads/";
+        private final String accessKey = "OJ5ePYgOmXHSdi7Wb8cebmB0OwDUDlGzeTeWORdH";
+        private final String secretKey = "ldPnyGLV5NeIwtzVLPTX-W-BXut7vMKtVXLglLao";
+        private final String bucket = "ness";
+
+
+        /**
+         * 获取上传凭证
+         *
+         * @return
+         */
+        private String getUploadToken() {
+
+            return Auth.create(accessKey, secretKey).uploadToken(bucket);
+
+        }
+
+        /**
+         * 根据文件名称上传文件
+         *
+         * @param fileName
+         */
+        private void upload(String fileName) throws QiniuException {
+            UploadManager uploadManager = new UploadManager(new Configuration(Zone.zone0()));
+
+            uploadManager.put(FILE_TOTAL_PATH + fileName
+                    , fileName, getUploadToken());
+
+
+        }
+
+
+    }
+
+
+}
---
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/order/controller/TestDo.java
@@ -0,0 +1,111 @@
+package com.e_commerce.miscroservice.order.controller;
+
+import com.e_commerce.miscroservice.commons.entity.application.TUser;
+import com.e_commerce.miscroservice.commons.helper.util.application.generate.TokenUtil;
+import com.e_commerce.miscroservice.commons.helper.util.colligate.encrypt.Md5Util;
+import com.e_commerce.miscroservice.commons.util.colligate.StringUtil;
+import com.e_commerce.miscroservice.commons.utils.UserUtil;
+import com.e_commerce.miscroservice.guanzhao_proj.product_order.service.GZLessonService;
+import com.e_commerce.miscroservice.guanzhao_proj.product_order.service.GZSubjectService;
+import com.e_commerce.miscroservice.user.service.UserService;
+import com.netflix.discovery.converters.Auto;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
+import sun.net.ftp.FtpClient;
+
+@Controller
+@Data
+public class TestDo {
+
+    @Autowired
+    private FileUrlManagers fileUrlManagers;
+
+    @Autowired
+    private GZSubjectService gzSubjectService;
+
+    @Autowired
+    private GZLessonService gzLessonService;
+
+    private final String EMPTY = "";
+    private final Pattern filePattern=Pattern.compile("(.*)\\.mp4$");
+
+
+    /**
+     * 推送
+     * @param subjectId
+     * @param fileName
+     */
+    @GetMapping("push")
+    @ResponseBody
+    public void push(Long subjectId, String fileName) {
+        log.info("开始推送文件={}",fileName);
+        Boolean isPushSuccess = fileUrlManagers.push(fileName);
+        if(isPushSuccess) {
+            gzLessonService.sendUnlockTask(subjectId, fileName);
+        }
+
+    }
+
+
+    @Autowired
+    private HttpServletRequest request;
+
+    /**
+     * 获取播放
+     *
+     * @param fileName 文件名称
+     * @return
+     */
+    @GetMapping("play" + TokenUtil.AUTH_SUFFIX)
+    public String play(String fileName, String sign, Long productId, Long lessonId) {
+        TUser user = UserUtil.getUser();
+        user = new TUser();
+        user.setId(42l);
+        user.setName("三胖");
+        if(StringUtil.isEmpty(fileName)||
+                !filePattern.matcher(fileName).matches()){
+
+            log.info("获取文件播放地址失败,文件名称={}不正确",fileName);
+            return EMPTY;
+        }
+
+        log.info("开始获取播放文件={}地址",fileName);
+        //权限校验
+        // TODO
+        gzLessonService.authCheck(sign, user.getId(), fileName, productId, lessonId);
+
+        request.setAttribute(fileName, Md5Util.md5(fileName));
+        return "forward:/doPlay";
+    }
+
+
+    /**
+     * 内部获取播放入口
+     *
+     * @param fileName
+     * @return
+     */
+    @GetMapping("doPlay")
+    @ResponseBody
+    public String doPlay(String fileName) {
+
+
+        Object hash = request.getAttribute(fileName==null?EMPTY:fileName);
+        if (hash == null ||
+                !Md5Util.md5(fileName).equals(hash)) {
+            return EMPTY;
+        }
+
+
+        return fileUrlManagers.getUrl(fileName);
+
+    }
+
+
+}
