4d13b0e0817d1d6e2b98709639fb1e1586a8a370
nessary <nessary@foxmail.com>
2019-05-31 09:46:11 +0800

    支付宝死要

--- miscroservice_users/src/main/java/com/e_commerce/miscroservice/push_controller/FileUrlManagers.java
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/push_controller/FileUrlManagers.java
@@ -106,7 +106,7 @@ public class FileUrlManagers {
 		private final String DNS_DOMAIN_NAME = "xiaoshitimebank.com";
 		private volatile Boolean IS_START = Boolean.FALSE;
 		private final String ACCESS_URL_PREFIX = "http://%s." + DNS_DOMAIN_NAME + "/%s";
-		private final String DNS_VAL_PARSE_DOMAIN_NAME = "xiaoshitimebank.com.www.qiniudns.com";
+		private final String DNS_VAL_PARSE_DOMAIN_NAME = "qiniu.xiaoshitimebank.com";
 
 		private static Map<String, Long> CDN_ID_CACHE = new ConcurrentHashMap<>();
 
