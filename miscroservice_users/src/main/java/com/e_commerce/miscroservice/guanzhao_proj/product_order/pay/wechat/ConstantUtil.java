7d80fba9e65ecb17e5d4f6417defcb6e6ba6766c
nessary <nessary@foxmail.com>
2019-06-03 14:19:50 +0800

    微信支付

--- miscroservice_users/src/main/java/com/e_commerce/miscroservice/guanzhao_proj/product_order/pay/wechat/ConstantUtil.java
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/guanzhao_proj/product_order/pay/wechat/ConstantUtil.java
@@ -5,11 +5,14 @@ public class ConstantUtil {
      * 微信开发平台应用ID
      */
     public static final String APP_ID = "wxb8edf6df645eb4e5";
+	/**
+	 * 微信开发平台应用app_secret
+	 */
+	public static final String APP_SECRET = "39f5d00a3fb73d82e817525564f99b25";
     /**
      * 应用对应的密钥
      */
-    public static final String APP_KEY = "XmhML5NXoU6dzN72GxVBrZJFmyXy0Ldu";
-//	wxb8edf6df645eb4e5
+    public static final String APP_KEY = "5uBcQ1wcsu8U46xEwgYxv68aRxqsRsLM";
     /**
      * 微信支付商户号
      */
@@ -22,5 +25,5 @@ public class ConstantUtil {
     /**
      * 微信服务器回调通知url
      */
-    public static String NOTIFY_URL = "";
+    public static String NOTIFY_URL = "https://test.xiaoshitimebank.com/user/wxAppPay/wxNotify";
 }
--- miscroservice_users/src/main/java/com/e_commerce/miscroservice/guanzhao_proj/product_order/pay/wechat/WxPayAppController.java
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/guanzhao_proj/product_order/pay/wechat/WxPayAppController.java
@@ -1,6 +1,5 @@
-package com.e_commerce.miscroservice.commons.util.colligate.pay;
+package com.e_commerce.miscroservice.guanzhao_proj.product_order.pay.wechat;
 
-import com.e_commerce.miscroservice.guanzhao_proj.product_order.pay.wechat.WeChatPay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,34 +9,43 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/wxAppPay")
+@RequestMapping("/wxPay")
 public class WxPayAppController {
 
 	@Autowired
 	WeChatPay weChatPay;
 
-    /**
-     * 回调函数
-     *
-     * @param request
-     * @param response
-     * @throws Exception
-     */
-    @RequestMapping("/wxNotify")
-    public void wxNotify(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        //解析参数
-        Map param = weChatPay.doParseRquest(request);
-        if ("SUCCESS".equals(param.get("return_code"))) {
-            // 如果返回成功
-            String mch_id = (String) param.get("mch_id"); // 商户号
-            String out_trade_no = (String) param.get("out_trade_no"); // 商户订单号
-            String total_fee = (String) param.get("total_fee");
-            // 查询订单 根据订单号查询订单
-            System.out.println("商户号" + mch_id + "out_trade_no" + out_trade_no + "total_fee" + total_fee);
+	/**
+	 * 回调函数
+	 *
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	@RequestMapping("/wxNotify")
+	public void wxNotify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//解析参数
+		Map param = weChatPay.doParseRquest(request);
+		if ("SUCCESS".equals(param.get("return_code"))) {
+			// 如果返回成功
+			String mch_id = (String) param.get("mch_id"); // 商户号
+			String out_trade_no = (String) param.get("out_trade_no"); // 商户订单号
+			String total_fee = (String) param.get("total_fee");
+			// 查询订单 根据订单号查询订单
+			System.out.println("商户号" + mch_id + "out_trade_no" + out_trade_no + "total_fee" + total_fee);
 
-            }
-        }
+		}
+	}
 
+
+	@RequestMapping("/pay")
+	public Map<String, String> getPaySign(HttpServletRequest request) throws Exception {
+
+
+		return weChatPay.createWebParam(System.currentTimeMillis() + "", 0.01, request);
+
+
+	}
 
 
 }
