db074c9b684cab613607aade9c732328ddb11549
nessary <nessary@foxmail.com>
2019-06-12 15:36:37 +0800

    微信支付

--- miscroservice_users/src/main/java/com/e_commerce/miscroservice/csq_proj/po/BaseEntity.java
+++ miscroservice_users/src/main/java/com/e_commerce/miscroservice/csq_proj/po/BaseEntity.java
@@ -7,21 +7,21 @@ import java.sql.Timestamp;
 
 public class BaseEntity {
 
-	@Column(commit = "扩展字段")
-	private String extend;
-
-	@Column(commit = "创建者编号", isNUll = false)
-	private Long createUser;
-
-	@Column(commit = "创建时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.CREATE)
-	private Timestamp createTime;
-
-	@Column(commit = "更新者编号", isNUll = false)
-	private Long updateUser;
-
-	@Column(commit = "更新时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.UPDATE)
-	private Timestamp updateTime;
-
-	@Column(commit = "有效性", defaultVal = "1")
-	private String isValid;
+//	@Column(commit = "扩展字段")
+//	private String extend;
+//
+//	@Column(commit = "创建者编号", isNUll = false)
+//	private Long createUser;
+//
+//	@Column(commit = "创建时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.CREATE)
+//	private Timestamp createTime;
+//
+//	@Column(commit = "更新者编号", isNUll = false)
+//	private Long updateUser;
+//
+//	@Column(commit = "更新时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.UPDATE)
+//	private Timestamp updateTime;
+//
+//	@Column(commit = "有效性", defaultVal = "1")
+//	private String isValid;
 }
