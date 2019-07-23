b833449cfe7dd1adb8767ab37ee8772ae3bff2e1
nessary <nessary@foxmail.com>
2019-07-23 17:17:53 +0800

    修复toSring()

--- test/src/main/resources/deploy/deploy.sh
+++ test/src/main/resources/deploy/deploy.sh
@@ -1,2 +1,2 @@
-result=`ssh -o StrictHostKeyChecking=no  -i ./test/src/main/resources/deploy/id_rsa  -p 11 root@timer.nessary.top "sh /opt/deploy/deploy_application.sh"`
+result=`ssh -o StrictHostKeyChecking=no  -i ./test/src/main/resources/deploy/id_rsa  -p 11 root@timer.p4j.cc "sh /opt/deploy/deploy_application.sh"`
 echo $result
