1378f5b76c8a21e80fd3fab0e7e324bf40255371
huangyangfeng <979470284@qq.com>
2019-10-17 14:43:29 +0800

    fix: 权限设计 提交

--- miscroservice_user/src/main/java/com/e_commerce/miscroservice/lpglxt_proj/po/BaseEntity.java
+++ miscroservice_user/src/main/java/com/e_commerce/miscroservice/lpglxt_proj/po/BaseEntity.java
@@ -8,21 +8,6 @@ import java.sql.Timestamp;
 @Data
 public class BaseEntity extends com.e_commerce.miscroservice.commons.entity.application.BaseEntity {
 
-	@Column(commit = "扩展字段")
-	protected String extend;
-
-	@Column(commit = "创建者编号", isNUll = false)
-	protected Long createUser;
-
-	/*@Column(commit = "创建时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.CREATE)
-	protected Timestamp createTime;*/
-
-	@Column(commit = "更新者编号")
-	protected Long updateUser;
-
-	/*@Column(commit = "更新时间戳", dateGeneStrategy = DbHandler.DateGeneStrategy.UPDATE)
-	protected Timestamp updateTime;*/
-
 	@Column(commit = "有效性", defaultVal = "1")
 	protected String isValid;
 }
